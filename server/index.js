89a0ac5ff8b5f9edc2ca5784cc8b167fc39ce058
MUHAMMAD SHAHWAIZ MUGHAL <shahwaizmughal02@gmail.com>
2024-11-26 19:06:57 +0500

    chngin urls

--- server/index.js
+++ server/index.js
@@ -20,7 +20,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://oracure.vercel.app",
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
   })
