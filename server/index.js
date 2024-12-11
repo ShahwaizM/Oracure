f7d1ed93e547e0c8f99876781e88f657a89daa60
MUHAMMAD SHAHWAIZ MUGHAL <86921257+ShahwaizM@users.noreply.github.com>
2024-12-12 00:50:53 +0500

    Update index.js

--- server/index.js
+++ server/index.js
@@ -24,6 +24,7 @@ app.use(
     origin: "https://oracure.vercel.app",
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true, 
   })
 );
 
