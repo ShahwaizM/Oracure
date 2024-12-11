60e8070f5aaf07a88e752c73c82c6ab05754118e
MUHAMMAD SHAHWAIZ MUGHAL <86921257+ShahwaizM@users.noreply.github.com>
2024-12-11 23:46:04 +0500

    Update main.jsx

--- Client/src/main.jsx
+++ Client/src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App.jsx";
+import App from "./App.js";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import Header from "./Components/Header.jsx";
